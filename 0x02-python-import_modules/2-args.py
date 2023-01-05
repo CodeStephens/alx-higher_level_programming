97826eac3a9519b58c67aeeca4eb878a5a7b763d
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-05 20:36:33 +0000

    code updated

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     argumentLength = len(sys.argv)
     if argumentLength == 1:
-        print("{} arguments.".format(argumentLength - 1)
+        print("{} arguments.".format(argumentLength - 1))
     else:
-        for i in range(1:argumentLength):
+        for i in range(1: argumentLength):
             print(f'{i}: {sys.argv[i]}')
