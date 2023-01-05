6cbccb5b83fb9a39e49f4c0e2874f3d6c0b6b6c2
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-05 21:53:11 +0000

    code updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     argumentLength = len(sys.argv) - 1
-    sum = 0;
+    sum = 0
     for i in range(argumentLength):
         sum += sys.argv[i + 1]
     print(f'{sum}')
