c35cd25f1988283ba42ded226a0be03036b74acb
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-05 21:51:59 +0000

    task3-infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argumentLength = len(sys.argv) - 1
+    sum = 0;
+    for i in range(argumentLength):
+        sum += sys.argv[i + 1]
+    print(f'{sum}')
