c86fe95ad3eb4cd1d3670cd55ee0f75946295b0d
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-05 20:10:19 +0000

    task2-how to make script dynamic

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    print("{} arguments:".format(len(argv)))
+    for i in range(len(argv)):
+        if i == 0:
+            continue
+        else:
+            print(f'{i}: {sys.argv[i]}'}
