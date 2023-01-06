7c8797e249df1e58a33adc0515b46bab06b733b5
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-06 07:33:57 +0000

    code updated

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     operator = sys.argv[2]
     b = int(sys.argv[3])
     if operator == '+':
-        print(f'{a} {operator} {b} = add(a, b)')
+        print(f'{a} {operator} {b} = {add(a, b)}')
     elif operator == '-':
-        print(f'{a} {operator} {b} = sub(a, b)')
+        print(f'{a} {operator} {b} = {sub(a, b)}')
     elif operator == '*':
-        print(f'{a} {operator} {b} = mul(a, b)')
+        print(f'{a} {operator} {b} = {mul(a, b)}')
     elif operator == '/':
-        print(f'{a} {operator} {b} = div(a, b)')
+        print(f'{a} {operator} {b} = {div(a, b)}')
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
