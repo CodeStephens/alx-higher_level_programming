78ee47cfab209e5ea5dc198e2253614e18f14c88
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-08 21:34:21 +0000

    modified task3 code

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if i < 0:
+        return
     i = len(my_list) - 1
     while i >= 0:
         print("{:d}".format(my_list[i]))
