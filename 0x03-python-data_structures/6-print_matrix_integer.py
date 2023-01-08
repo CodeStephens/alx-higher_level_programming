9deade213188a91a44549e018feb00ec8aabacd1
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-08 23:44:46 +0000

    modified task6 code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,7 @@
 
 def print_matrix_integer(matrix[[]]):
     for i in range(len(matrix)):
-        for j in range(len(matrix[i]):
-                print("{:d}".format(matrix[i][j], end ' ')
-                if j == len(matrix[i]):
-                    print()
-
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j], end ' '))
+            if j == len(matrix[i]):
+                print()
