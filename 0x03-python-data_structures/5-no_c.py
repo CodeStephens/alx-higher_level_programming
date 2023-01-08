00f3a71e3f4cac65b03776695a8d84dce551d4b5
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-08 23:30:06 +0000

    modified task5 code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 
 def no_c(my_string):
     copy_my_string = [let for let in my_string if let != 'c' and let != 'C']
-    ''.join(copy_my_string)
+    copy_my_string = ''.join(copy_my_string)
     return (copy_my_string)
