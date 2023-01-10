64c095f6c08933d7bc84ecbbe868d6bb162e4010
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-10 19:54:23 +0000

    modified task3 code

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    reverse_list = []
-    reverse_list = my_list.reverse_list()
-    for i in reverse_list:
-        print("{:d}".format(my_list[i]))
+     my_list.reverse_list()
+    for i in my_list:
+        print("{:d}".format(i))
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -3,7 +3,6 @@
 
 	#include <stdio.h>
 	#include <stdlib.h>
-	#include <CPython.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
