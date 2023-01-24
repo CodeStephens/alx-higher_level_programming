792441f883cacf60b4f9fbda3dc6b45ec08334f6
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-24 04:35:19 +0000

    modified code

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
-# 4-list_division.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 
 def list_division(my_list_1, my_list_2, list_length):
-    """Divides two lists element by element.
-
-    Args:
-        my_list_1 (list): The first list.
-        my_list_2 (list): The second list.
-        list_length (int): The number of elements to divide.
-
-    Returns:
-        A new list of length list_length containing all the divisions.
-    """
     new_list = []
     for i in range(0, list_length):
         try:
--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-# 5-raise_exception.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 
 def raise_exception():
-    """Raise a TypeError exception."""
     raise TypeError
