ca046e30fe074d039086083aa73d9f10d3fd6b16
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-01-31 23:35:33 +0000

    task101 modified

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(n):
     for i in range(n):
-        print("BestSchool " * i)
+        print("BestSchool, " * i)
--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,9 +2,7 @@
 
 
 class LockedClass:
-    """ dynamically prohibits the creation of instance attribute without
-    reference to 'first_name'
-    """
     __slots__ = ['first_name']
+
     def __init__(self, first_name):
         self.first_name = first_name
