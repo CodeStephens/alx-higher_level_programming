71d04ca7629f429b460532cb230ba127f797da31
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-02-09 19:40:26 +0000

    modified task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """writes text content to a filename of a UTF8 text file"""
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(text)
+        return (f.write(text))
