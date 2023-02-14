06f8313a02f96a80e29fff4108eb1cfe012bf499
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-02-14 04:52:35 +0000

    more tasks

---
+++ 0x0C-python-almost_a_circle/models/2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3 
+""" defining the base class required for the 'almost a circle' project """
+
+
+class Base:
+    """defining private class attribute and instance attributes """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+    """ initialiazing the class """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
