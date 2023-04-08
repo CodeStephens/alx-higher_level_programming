a53cae57584e19ea29a6396e293365c8d7050adc
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-08 23:20:48 +0000

    Python inheritance tasks 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines a class BaseGeometry with a public instance method
+"""
+
+
+class BaseGeometry():
+    """
+    This is an empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
