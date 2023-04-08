bc7e33021de4388faf7fab8921dc8ade384cbb79
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-08 23:00:47 +0000

    updated codes

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,11 +4,11 @@ Defines a function that checks the subclass identity of a given object
 """
 
 
-def inherits_fom(obj, a_class):
+def inherits_from(obj, a_class):
     """
     the checks if obj is an instance of a class that inherited directly or
     indirectly from a given class
     """
-    if issubclass(type(obj), a_class) and (type(obj) != a_class):
+    if issubclass(type(obj), a_class):
         return (True)
     return (False)
