a9d9e6e163e26ae438eddd1129da3751d0ec8c81
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-08 22:56:06 +0000

    Python inheritance tasks 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines a function that checks the subclass identity of a given object
+"""
+
+
+def inherits_fom(obj, a_class):
+    """
+    the checks if obj is an instance of a class that inherited directly or
+    indirectly from a given class
+    """
+    if issubclass(type(obj), a_class:
+        return (True)
+    return (False)
