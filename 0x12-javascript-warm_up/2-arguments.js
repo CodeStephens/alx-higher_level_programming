da1176e13121ba7838d904774a76b5da1f2c7f10
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-12 04:39:37 +0000

    updated codes

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 // Script prints distinct message based on the number of arguments passed
 const process = require('process');
 let args = process.argv;
-if (args.length === 2) {
+if (args.length <= 2) {
   console.log('No argument');
 } else if (args.length === 3) {
   console.log('Argument found');
