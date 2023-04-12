2b57378034ec497dfbae8baefe292214aaa37e06
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-12 04:26:32 +0000

    Javascript-warm_up tasks5

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node 
 // Script prints distinct message based on the number of arguments passed
-
-
 const process = require('process');
 let args = process.argv;
 if (args.length === 2) {
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node 
 // Script prints distinct message based on the number of arguments passed
-
-
 const process = require('process');
 let args = process.argv.slice(2);
 if (args.length === 0) {
