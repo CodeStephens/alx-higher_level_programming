89a40c0410ba116bc41060029e5f530bd5bb6c6d
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-12 12:36:43 +0000

    Javascript-warm_up tasks7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require('process');
+const argsLength = process.argv.length;
+if ((argsLength < 3) || (isNaN(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+}
