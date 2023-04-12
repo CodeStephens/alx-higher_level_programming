68d9b045006c5aeffbd74f719967d7ddc1af3773
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-12 05:03:10 +0000

    updated codes

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -2,4 +2,4 @@
 // Script prints distinct message based on the number of arguments passed
 const process = require('process');
 let args = process.argv.slice(2);
-console.log(args[0] + 'is' + args[1]);
+console.log(args[0] + ' is ' + args[1]);
