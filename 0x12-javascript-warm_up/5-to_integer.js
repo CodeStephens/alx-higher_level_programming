0a75fcc2432e825af4f5acf298c88236d8e23fd1
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-12 08:57:06 +0000

    updated codes

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // Script prints distinct message based on the number of arguments passed
 const process = require('process');
-let args = process.argv.slice(2);
+const args = process.argv.slice(2);
 if (args.length === 0) {
-  console.log('Not a Number');
+  console.log('Not a number');
 } else if (isNaN(args[0])) {
-  console.log('Not a Number');
+  console.log('Not a number');
 } else {
   console.log(parseInt(args[0]));
 }
