8f2a9b9f6911b1b23b6c6185fbd5aea89971d651
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-13 19:03:44 +0000

    updated codes

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,15 +1,5 @@
 #!/usr/bin/node
 // Script prints distinct message based on the number of arguments passed
 const process = require('process');
-const cArguments = process.argv.slice(2);
-let count = 0;
-let i = 0;
-while (cArguments[i] !== (null || undefined)) {
-  count++;
-  i++;
-}
-if (count === 0) {
-  console.log('No Argument');
-} else {
-  console.log(cArguments[0]);
-}
+const firstArgType = typeof process.argv[2];
+console.log(firstArgType === 'undefined' ? 'No argument' : process.argv[2]);
