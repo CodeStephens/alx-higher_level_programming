5850bf91baea146be548822a014953a9caf0905a
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-13 19:53:21 +0000

    updated codes

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 const process = require('process');
-function factorial(process.argv[2]) {
-  const num = pasrseInt(process.argv[2]);
-  if (isNaN(num)) {
-    console.log('1');
-  } else {
-    console.log(num * factorial(num - 1));
+const num = parseInt(process.argv[2]);
+
+if (isNaN(num)) {
+  console.log('1');
+} else {
+  console.log(factorial(num));
 }
-factorial()
+
+function factorial (num) {
+  if (num === 0) {
+    return (1);
+  } else {
+    return (num * factorial(num - 1));
+  }
+}
