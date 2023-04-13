e73ebe5aa070bfe5c98d720b15527b079c975dbb
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-13 18:36:14 +0000

    updated codes

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Script prints distinct message based on the number of arguments passed
 const process = require('process');
-const cArguments = process.argv.slice(2);
+let cArguments = process.argv.slice(2);
 let count = 0;
 let i = 0;
 while (cArguments[i] != null) {
