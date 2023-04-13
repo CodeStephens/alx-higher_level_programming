c01d2a5090e2cc2831e794c581bff1a6a47cbdb4
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-13 22:51:46 +0000

    updated codes

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,7 +5,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr() = function() {this.value++};
+myObject.incr() = function() {
+  this.value++;
+}
 myObject.incr();
 console.log(myObject);
 myObject.incr();
