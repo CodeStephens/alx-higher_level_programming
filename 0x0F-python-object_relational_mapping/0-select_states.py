c07d131af626b9f04a66fa9ffb19507556d1da67
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-16 21:01:58 +0000

    task0 & README.md

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# Python script listing all states from a database hbtn_0e_0_usa
+
+
+# import needed module for the functionality of the script's purpose
+import MySQLdb
+from sys import argv
+
+# establishing a database connection
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+
+    # creating an environment for implementation of database queries
+    cursor = db.cursor()
+
+    # executing MYSAL query to list all states
+    cursor.execute("SELECT * FROM states ORDER BY state.id ASC")
+
+    # fetch and printing out each data row from the database
+    rows = cursor.fetchall()
+    for row_s in rows:
+        print(row)
+
+    # closing connection
+    cursor.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,12 @@
+# Python - Object-Relational Mapping
+
+## Overview
+This directory contains explicit work on interaction of python and database
+(MySQL), as well as the role of Object Relational Mapper (ORM) such as
+SQLAlchemy in actively being a middle-man between python and databses.
+
+## Technologies
+1. Python
+2. MySQL
+3. MySQLdb
+4. SQLAlchemy
