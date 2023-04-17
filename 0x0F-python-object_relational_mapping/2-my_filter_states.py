836a3c4b548951b6c5c8bff9aaa301b405a997ca
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-17 16:57:09 +0000

    task2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Python script listing states correspodnding to a state searched keyword
+"""
+
+
+# import needed module for the functionality of the script's purpose
+import MySQLdb
+from sys import argv
+
+# establishing a database connection
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3])
+
+    searchedWord = argv[4]
+
+    # creating an environment for implementation of database queries
+    cursor = db.cursor()
+
+    # executing MYSQL query to list all states
+    cursor.execute("SELECT * FROM states\
+            WHERE name = '{}' \
+            ORDER BY states.id ASC".format(searchedWord))
+
+    # fetch and print out each data row from the database
+    rows = cursor.fetchall()
+    for row_s in rows:
+        print(row_s)
+
+    # closing connection
+    cursor.close()
+    db.close()
