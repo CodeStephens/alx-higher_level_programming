af98e500cc4b5ad8ec0b85423ad51aca04d8dd8e
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-21 23:43:02 +0000

    added task6-using sqlalchemy to connect to database

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    tid = Column("id", Integer, primary_key=True, autoincrement=True,
+                nullable=False)
+    name = Column("name", String(128), nullable=False)
+
+
+engine = create_engine('mysql://root:Mestika1*@localhost:3306/hbtn_0e_6_usa')
+Base.metadata.create_all(engine)
