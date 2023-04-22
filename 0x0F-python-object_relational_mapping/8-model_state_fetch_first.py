37e2d3477112a13093a86816c9f0b719075ddf58
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-22 15:13:16 +0000

    updated 8-model_state_fetch.first.py code

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -18,4 +18,7 @@ if __name__ == '__main__':
     session = Session()
 
     result = session.query(State).first()
-    print(f"{result.id}: {result.name}")
+    if result:
+        print(f"{result.id}: {result.name}")
+    else:
+        print('Nothing')
