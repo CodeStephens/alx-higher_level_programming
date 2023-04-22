6101a99bf02303add78293fefb12634cc83fac62
“Oluwatosin” <oluwatosin.stephens@gmail.com>
2023-04-22 14:51:52 +0000

    updated 9-model_state_filter_a.py code

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -17,7 +17,8 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).order_by(State.id).filter
-    (State.name.like('%a%'))
+    states = session.query(State).filter(State.name.like('%a%')).order_by
+    (State.id).all()
+
     for state in states:
-        print(f"{state.id}: {state.name}")
+        print(f'{state.id}: {state.name}')
