2448bb9fc0988f014162cdc95d8305fd619429f6
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-01 21:52:21 +0100

    updated task1 code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script attempts to print out the content of the url its trying to reach
-if [ $(curl -s -o /dev/null -w "%{http_code}" "$1") -eq 200 ]; then echo "$(curl -s "$1")"; else echo ""; fi 
+echo "$(curl -s -L "$1")"
