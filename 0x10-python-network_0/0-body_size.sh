5155ac29e6d7baf9d0c0df73316a18b970d2f345
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-01 15:08:23 +0100

    updated code

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# This script attempts to take a (URL) argument; while a URL is sent to the argument, the size of the content in bytes is sought for
-
-if [ -z "$1" ]; then
-	echo "please enter a valid url"
-	exit 1
-fi
-
-port=$(echo "$1" | awk -F: '{print $2}')
-if [ $port = "5000" ]; then
-	size_of_download=$(curl -s -w '%{size_download}' -o /dev/null $1)
-	echo ${size_of_download}
-fi
+size_of_download=$(curl -s -w '%{size_download}' -o /dev/null $1)
+echo ${size_of_download}
