c3f583e75bc7dc7f5ae4fecdb181e52c550d257e
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-02 23:42:18 +0100

    task0 code updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ url = "https://alx-intranet.hbtn.io/status"
 
 
 if __name__ = '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print('Body response:')
