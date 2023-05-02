bb115c39031880a5bf64a5e1cb7647f60493f8e0
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-02 22:59:48 +0100

    updated task9-You got me! code

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# HTTP request POST method on JSON payload
-curl -sL -X PUT "0.0.0.0:5000/catch_me" -H "Origin: X-School" -d "User-Id=98"
+# HTTP request PUT method "You got me!"
+curl -so /dev/null -w "You got me!" 0.0.0.0:5000/catch_me
