105a4c61f4a4c3c88eab0b906a8ea46d6e17a63e
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-05 21:08:28 +0100

    task7 - error_code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This Python script fetches a URL which displays body of the response
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    html_ response = requests.get(argv[1])
+    if html_response.status_code >= 400:
+        print("Error code:{}".format(html_response.status_code))
+    else:
+        print(html_response.text)
