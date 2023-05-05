fcec1e8189ec2124a60187999f3da6a010d4b0c1
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-05 16:15:24 +0100

    task3 - Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This Python script fetches a URL which displays body of the response
+"""
+import urllib.request
+from sys import argv
+from urllib.error import URLError
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(url) as response:
+        feedback = response.read()
+        print(feedback.decode('utf-8'))
+    except URLError as e:
+        print('Error code ', e.code)
