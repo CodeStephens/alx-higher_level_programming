189e93ffcae85a2aed04dad8c82a159ddf8e2fe0
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-05 21:15:59 +0100

    task7 code updated

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ from sys import argv
 if __name__ == "__main__":
     html_response = requests.get(argv[1])
     if html_response.status_code >= 400:
-        print("Error code:{}".format(html_response.status_code))
+        print("Error code: {}".format(html_response.status_code))
     else:
         print(html_response.text)
