d5c864537ffd7ed1adcbf42c6f4956c4fb583057
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-06 10:15:39 +0100

    task updated

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
 """
-This python scripts intend to retrieve GitHud id via GitHub API using
-GitHub credentials (username and password) as command line argument to
-authenticate
+Python script that takes GitHub credentials
+and uses the GitHub API to display the user id
 """
-import requests
-from sys import argv
+from requests import get, auth
+import sys
 
 
-# Get GitHub credentials from command line arguments
-username = argv[1]
-password = argv[2]
-
-# Set up the API endpoint and headers
-url = 'https://api.github.com/user'
-headers = {'Accept': 'application/vnd.github.v3+json'}
-
-# Make the API request using Basic Authentication
-response = requests.get(url, auth=(username, password), headers=headers)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Print the user ID
-    user_id = response.json()['id']
-    print(f"{user_id}")
-else:
-    print("Failed to get user ID. Check your credentials and try again.")
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r)
+    print(r.json().get('id'))
