dfacb53bde43964ddfce4998f78535e2312b59c1
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-07 11:33:11 +0100

    updated code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,6 +8,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
+    """ using GitHub API to fetch commit history of a repository """
     try:
         url = 'https://api.github.com/repos/{}/{}/commits'.\
                 format(argv[2], argv[1])
