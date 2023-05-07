133a41284c0b370a511497c76969ce0d6530c131
Oluwatosin Stephen <oluwatosin.stephens@gmail.com>
2023-05-07 11:29:40 +0100

    task10 - time for an interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This python script displays the last 10 commits of a repository (rails) by
+user (rails) in Github using GitHub API
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        url = 'https://api.github.com/repos/{}/{}/commits'.\
+                format(argv[2], argv[1])
+        response = get(url)
+        response_json = response.json()
+        for i in range(0, 10):
+            print("{}: {}".format(response_json[i].get('sha'),
+                                  response_json[i].get('commit').get('author\
+                                  ').get('name')))
