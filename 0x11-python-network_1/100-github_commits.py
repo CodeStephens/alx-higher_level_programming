94f1c03885e9439ad3c3a95eb53ea5901525a360
CodeStephens <oluwatosin.stephens@gmail.com>
2023-07-05 17:10:13 +0100

    updated task10 code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,12 +12,9 @@ if __name__ == "__main__":
     using GitHub API to fetch commit history of a repository
     """
 
-    try:
-        url = 'https://api.github.com/repos/{}/{}/commits'.\
-                format(argv[2], argv[1])
-        response = get(url)
-        response_json = response.json()
-        for i in range(0, 10):
-            print("{}: {}".format(response_json[i].get('sha'),
-                                  response_json[i].get('commit').get('author\
-                                  ').get('name')))
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+    response = get(url)
+    commits = response.json()
+    for i in range(0, 10):
+        print("{}: {}".format(commits[i].get('sha'), commits[i].get('commit').
+                              get('author').get('name')))
